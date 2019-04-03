0744a083fec9a8df6802af86fa4ee4224bd2402b
Rane Wallin <RAN2143066@maricopa.edu>
2019-04-02 18:07:18 -0700

    set up api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+import Friends from './components/Friends';
+
+
 
 class App extends Component {
+  state = {
+    friends: []
+  }
+
+  componentDidMount() {
+    axios
+        .get("http://localhost:5000/friends")
+        .then(resp => {
+          this.setState({ friends: resp.data })
+        })
+        .catch(console.log)
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Friends friends={this.state.friends} />
       </div>
     );
   }
---
+++ src/components/Friends.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Friends = props => {
+
+}
+
+export default Friends;
