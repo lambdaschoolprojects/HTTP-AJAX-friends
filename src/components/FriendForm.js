cf9851bd0f4533d752d67971228c70ac13d1dfe0
Rane Wallin <RAN2143066@maricopa.edu>
2019-04-04 18:06:35 -0700

    Day 2 - making form controlled

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -9,12 +9,22 @@ class FriendForm extends Component {
         }
     }
 
+    changeHandler = e => {
+        let { name, value } = e.target;
+        this.setState((prevState) => {
+            prevState.friend[name] = value;
+
+            return prevState;
+        })
+    }
+
     render() {
+        let { name, age, email } = this.state.friend;
         return (
             <form>
-                <input type="text" name="name" placeholder="Name" />
-                <input type="text" name="age" placeholder="Age" />
-                <input type="text" name="email" placeholder="Email" />
+                <input type="text" name="name" placeholder="Name" onChange={ this.changeHandler } value={name}/>
+                <input type="text" name="age" placeholder="Age" onChange={ this.changeHandler } value={age}/>
+                <input type="text" name="email" placeholder="Email" onChange={ this.changeHandler } value={email} />
                 <button>Add friend</button>
             </form>
         );
